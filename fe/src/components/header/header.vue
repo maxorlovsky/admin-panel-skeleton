5255150d0d7a871058a41f5c79dac74d49f6b219
Maxtream <max.orlovsky@gmail.com>
2017-09-04 13:53:18 +0200

    feat(multibrand): add multibrand switcher

--- fe/src/components/header/header.vue
+++ fe/src/components/header/header.vue
@@ -6,22 +6,44 @@
 
     <router-link to="/dashboard" class="logo"></router-link>
 
-    <!-- <select class="multi-brand">
-        <option></option>
-    </select> -->
+    <select class="multi-brand" v-if="enableBrands">
+        <option v-for="brand in multisite"
+            :value="brand.id"
+            v-bind:key="brand.id"
+        >{{brand.name}}</option>
+    </select>
 </header>
 </template>
 
 <script>
+// Website custom config
+import websiteConfig from '../../../../../mocms/config.json';
+
+// 3rd party libs
+import axios from 'axios';
+
 export default {
     name: 'header-component',
     data: function() {
         return {
             burgerStatus: this.$parent.leftSideMenu,
-            loggedIn: false
+            loggedIn: false,
+            enableBrands: websiteConfig.multiBrands,
+            multisite: []
         };
     },
+    created: function() {
+        return this.fetchData();
+    },
     methods: {
+        fetchData: function() {
+            const self = this;
+
+            axios.get('/api/multisite')
+            .then(function (response) {
+                self.multisite = response.data;
+            });
+        },
         burgerMenu: function() {
             this.$emit('nav-menu');
         }
