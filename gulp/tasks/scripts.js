2ba1914129272758593ef8f6eafeaadfcbf6f51a
Maxtream <max.orlovsky@gmail.com>
2017-09-18 18:24:17 +0200

    chore: version bump + updating all packages, replace gulp-webpack with webpack-stream

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,13 +3,13 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const runSequence = require('run-sequence');
-const gwebpack = require('gulp-webpack');
+const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 
 gulp.task('scripts', ['custom-modules'], () => {
 	const webpackConfig = require('../../webpack.config.js');
 
     return gulp.src('./vendor/fe/src/main.js')
-    	.pipe(gwebpack(webpackConfig, webpack))
+    	.pipe(webpackStream(webpackConfig))
         .pipe(gulp.dest('./dist/js/'));
 });
