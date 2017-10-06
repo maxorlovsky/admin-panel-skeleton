b4fc8960ada23279cef948c5ddc4757ea8b77754
maxorlovsky <max.orlovsky@gmail.com>
2017-10-06 07:40:45 +0200

    feat(client-node_modules): add option to load scripts from outside of cms node_modules

--- gulp/tasks/custom-modules.js
+++ gulp/tasks/custom-modules.js
@@ -4,6 +4,7 @@ const concat = require('gulp-concat');
 const rename = require("gulp-rename");
 const runSequence = require('run-sequence');
 const fs = require('fs');
+const config = require('../../../../../mocms/config.json'); // Fetching client config params
 
 gulp.task('custom-modules', (cb) => {
 	return runSequence(
@@ -14,14 +15,16 @@ gulp.task('custom-modules', (cb) => {
 // Copy VUE client components
 gulp.task('custom-modules:copy:vue', () => {
     return gulp.src('../../../mocms/components/**/*.vue')
-        .pipe(replace(/..\/vendor\/fe\/src\//g, ''))
+        .pipe(replace(/..\/vendor\/fe\/src\//g, '')) // Replace path for custom-modules to point correctly to CMS modules
+        .pipe(replace(/..\/..\/..\/node_modules/g, config.nodeModulesPath))  // Replace path for custom-modules to point correctly from CMS modules to client node_modules
     	.pipe(gulp.dest('./fe/src/custom-components'));
 });
 
 // Copy main file of client components
 gulp.task('custom-modules:copy:custom', () => {
     return gulp.src('../../../mocms/custom.js')
-        .pipe(replace(/\/vendor\/fe\/src/g, ''))
+        .pipe(replace(/\/vendor\/fe\/src/g, ''))  // Replace path for custom-modules to point correctly to CMS modules
+        .pipe(replace(/..\/..\/..\/node_modules/g, config.nodeModulesPath))  // Replace path for custom-modules to point correctly from CMS modules to client node_modules
         .pipe(replace(/components\//g, ''))
     	.pipe(gulp.dest('./fe/src/custom-components'));
 });
