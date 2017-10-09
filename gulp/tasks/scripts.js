55de966e4a68fdc549cb309d4bb4e8c4a4cedf9c
Maxtream <max.orlovsky@gmail.com>
2017-10-09 13:53:57 +0200

    fix(internet-explorer): add ie-fix file for IE11, small cleanup of gulp files

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,10 +1,5 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const runSequence = require('run-sequence');
 const webpackStream = require('webpack-stream');
-const webpack = require('webpack');
 
 gulp.task('scripts', ['custom-modules'], () => {
 	const webpackConfig = require('../../webpack.config.js');
