006fd54f39958d3fa3aff69e5a5fdb8145ea3e62
maxorlovsky <max.orlovsky@gmail.com>
2017-10-20 07:29:44 +0200

    feat(gulp-bump-ver): getting version from client config, if it doesn't exist take from CMS package.json

--- gulp/tasks/bump-ver.js
+++ gulp/tasks/bump-ver.js
@@ -1,16 +1,23 @@
 const gulp = require('gulp');
 const fs = require('fs');
-
 const packageJson = require('../../package.json');
+const config = require('../../../../../mocms/config.json'); // Fetching client config params
+let version = '';
+
+if (config.version) {
+    version = config.version;
+} else {
+    version = packageJson.version;
+}
 
 gulp.task("bump-ver", () => {
     const componentFilePath = './dist/js/vendors.js';
 	let componentsJsTemp = fs.readFileSync(componentFilePath, 'utf-8');
-    let componentJs = componentsJsTemp.replace('%version%', packageJson.version);
+    let componentJs = componentsJsTemp.replace('%version%', version);
     fs.writeFileSync(componentFilePath, componentJs);
 
     const indexFilePath = './dist/index.html';
 	let indexFileTemp = fs.readFileSync(indexFilePath, 'utf-8');
-    let indexFile = indexFileTemp.replace(/%version%/g, packageJson.version);
+    let indexFile = indexFileTemp.replace(/%version%/g, version);
     fs.writeFileSync(indexFilePath, indexFile);
 });
