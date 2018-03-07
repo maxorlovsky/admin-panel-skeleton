f307bdebe53e1bf9ae0b05b05e8bafee74a40211
Max Orlovsky <max.orlovsky@gmail.com>
2018-03-07 10:27:41 +0100

    fix(db-check): typo fix

--- api/middleware/db-check.php
+++ api/middleware/db-check.php
@@ -3,7 +3,7 @@ $app->add(function ($request, $response, $next) {
     // If no connection, send proper message as auth checked used everywhere
     if (!$this->db) {
         $response = $response->withStatus(503);
-        $data = array('message' => 'No database connection denied');
+        $data = array('message' => 'No database connection, denied');
 
         return $response->withJson($data, null, JSON_NUMERIC_CHECK);
     }
