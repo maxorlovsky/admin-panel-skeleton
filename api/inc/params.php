a6b07df72197d38b0ad567c5a71429f52d2b6086
Maxtream <max.orlovsky@gmail.com>
2018-07-22 00:59:49 +0200

    feat(params): add localhost as dev environment

--- api/inc/params.php
+++ api/inc/params.php
@@ -7,7 +7,7 @@ $container['params'] = function($c) {
 
     // Defining environment
     $breakDown = explode('.', $_SERVER['HTTP_HOST']);
-    if ($breakDown[0] == 'dev') {
+    if ($breakDown[0] == 'dev' || strpos($_SERVER['HTTP_HOST'], 'localhost') >= 0) {
         // Development environment
         $availableParams['env'] = 'dev';
     } else if ($breakDown[0] == 'test') {
